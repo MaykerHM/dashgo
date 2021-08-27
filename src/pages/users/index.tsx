356344d12d302af1a13a2b5a72f73682dbf83055
MaykerHM <hydeki8@gmail.com>
2021-08-27 19:00:25 -0300

    react query devtools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,7 +13,22 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+
+    return users
+  }, {
+    staleTime: 1000 * 5 // 5 segundos
   })
 
   const isWideVersion = useBreakpointValue({
@@ -73,18 +88,22 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={ ["4", "4", "6"] }>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold" >Mayker Miyanaga</Text>
-                        <Text fontSize="sm" color="gray.300" >hydeki8@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    { isWideVersion && <Td>04 de Abril, 2021</Td> }
-                  </Tr>
+                  { data.map(user => {
+                    return (
+                      <Tr key={ user.id }>
+                        <Td px={ ["4", "4", "6"] }>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold" >{ user.name }</Text>
+                            <Text fontSize="sm" color="gray.300" >{ user.email }</Text>
+                          </Box>
+                        </Td>
+                        { isWideVersion && <Td>{ user.createdAt }</Td> }
+                      </Tr>
+                    )
+                  }) }
                 </Tbody>
               </Table>
 
