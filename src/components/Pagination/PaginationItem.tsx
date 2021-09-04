2d02c4a35e45c9709f0f03aabace0d03278eb448
MaykerHM <hydeki8@gmail.com>
2021-09-04 20:00:28 -0300

    data fetching e cache local - complete

--- src/components/Pagination/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem.tsx
@@ -3,9 +3,14 @@ import { Button } from "@chakra-ui/react";
 interface PaginationItemProps {
   number: number
   isCurrent?: boolean
+  onPageChange: (page: number) => void
 }
 
-export function PaginationItem({ isCurrent = false, number }: PaginationItemProps) {
+export function PaginationItem({
+  isCurrent = false,
+  onPageChange,
+  number
+}: PaginationItemProps) {
   if (isCurrent) {
     return (
       <Button
@@ -19,7 +24,7 @@ export function PaginationItem({ isCurrent = false, number }: PaginationItemProp
           cursor: 'default'
         } }
       >
-        {number }
+        { number }
       </Button>
     )
   }
@@ -32,8 +37,9 @@ export function PaginationItem({ isCurrent = false, number }: PaginationItemProp
       _hover={ {
         bg: 'gray.500'
       } }
+      onClick={ () => onPageChange(number) }
     >
-      {number }
+      { number }
     </Button>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
-import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
+import { QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import { theme } from '../styles/theme'
-
-import { QueryClient, QueryClientProvider } from 'react-query'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawercontext'
 import { makeServer } from '../services/mirage'
+import { queryClient } from '../services/queryClient'
+import { theme } from '../styles/theme'
+
 
 if (process.env.NODE_ENV === 'development') {
   makeServer()
 }
 
-const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
